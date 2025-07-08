dffde29f3eb98747f324b8e2728ee7868d264dad
shubhamvermaa <vshubham0710@gmail.com>
2025-07-08 08:07:30 +0530

    spotlight automation

---
+++ spotlight.py
@@ -0,0 +1,25 @@
+import pyautogui
+import time
+from buttonAdvance import pressButtonAdvance, isThereButtonAdvance, pressMiddleScreen, pressNitroButton
+
+
+
+def main():
+    print("Spotlight automation script started.")
+    time.sleep(4)
+    pressButtonAdvance(r"Assets\Images\spotlightEvent.png", confidence=0.7, retries=3, delay=1.0, ignorePanic=True)
+    pressButtonAdvance(r"Assets\Images\spotlightEvent.png", confidence=0.7, retries=3, delay=1.0, ignorePanic=True)
+    pressButtonAdvance(r"Assets\Images\spotlightPlayButton.png", confidence=0.7, retries=3, delay=1.0, ignorePanic=True)
+    pressButtonAdvance(r"Assets\Images\raceButton.png", confidence=0.7, retries=3, delay=1.0, ignorePanic=True)
+    pressButtonAdvance(r"Assets\Images\saleenS1.png", confidence=0.7, retries=3, delay=1.0, ignorePanic=True)
+    pressButtonAdvance(r"Assets\Images\play1.png", confidence=0.7, retries=3, delay=1.0, ignorePanic=True)
+    for i in range(1, 120):
+        pressButtonAdvance(r"Assets\Images\star.png", confidence=0.7, retries=3, delay=1.0, ignorePanic=True)
+        if i % 8 == 0:
+            pressNitroButton()
+        time.sleep(1)
+         
+
+
+if __name__ == "__main__":
+    main()
