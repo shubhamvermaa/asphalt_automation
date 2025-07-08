5f7fa41adf9570f8d2c7ff329369511409eb2b66
shubhamvermaa <vshubham0710@gmail.com>
2025-07-08 08:16:34 +0530

    handle post spotlight

--- spotlight.py
+++ spotlight.py
@@ -10,14 +10,20 @@ def main():
     pressButtonAdvance(r"Assets\Images\spotlightEvent.png", confidence=0.7, retries=3, delay=1.0, ignorePanic=True)
     pressButtonAdvance(r"Assets\Images\spotlightEvent.png", confidence=0.7, retries=3, delay=1.0, ignorePanic=True)
     pressButtonAdvance(r"Assets\Images\spotlightPlayButton.png", confidence=0.7, retries=3, delay=1.0, ignorePanic=True)
-    pressButtonAdvance(r"Assets\Images\raceButton.png", confidence=0.7, retries=3, delay=1.0, ignorePanic=True)
-    pressButtonAdvance(r"Assets\Images\saleenS1.png", confidence=0.7, retries=3, delay=1.0, ignorePanic=True)
-    pressButtonAdvance(r"Assets\Images\play1.png", confidence=0.7, retries=3, delay=1.0, ignorePanic=True)
-    for i in range(1, 120):
-        pressButtonAdvance(r"Assets\Images\star.png", confidence=0.7, retries=3, delay=1.0, ignorePanic=True)
-        if i % 8 == 0:
-            pressNitroButton()
-        time.sleep(1)
+    while True:
+        pressButtonAdvance(r"Assets\Images\raceButton.png", confidence=0.7, retries=3, delay=1.0, ignorePanic=True)
+        pressButtonAdvance(r"Assets\Images\saleenS1.png", confidence=0.7, retries=3, delay=1.0, ignorePanic=True)
+        pressButtonAdvance(r"Assets\Images\play1.png", confidence=0.7, retries=3, delay=1.0, ignorePanic=True)
+        for i in range(1, 120):
+            pressButtonAdvance(r"Assets\Images\star.png", confidence=0.7, retries=3, delay=1.0, ignorePanic=True)
+            if i % 8 == 0:
+                pressNitroButton()
+            time.sleep(1)
+        pressButtonAdvance(r"Assets\Images\nextButton.png", confidence=0.7, retries=3, delay=1.0, ignorePanic=True)
+        pressButtonAdvance(r"Assets\Images\nextButton.png", confidence=0.7, retries=3, delay=1.0, ignorePanic=True)
+        pressButtonAdvance(r"Assets\Images\missoutButton.png", confidence=0.7, retries=3, delay=1.0, ignorePanic=True)
+        pressMiddleScreen()
+        pressButtonAdvance(r"Assets\Images\whiteNextButton.png", confidence=0.7, retries=3, delay=1.0, ignorePanic=True)
          
 
 
